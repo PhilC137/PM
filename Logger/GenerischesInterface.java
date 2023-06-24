5beb608027311cff687e701124572e41bde6d7f1
derPascalD <pascal2122@gmx.de>
2023-06-24 15:16:24 +0200

    Generisches Interface kommentiert

--- Logger/GenerischesInterface.java
+++ Logger/GenerischesInterface.java
@@ -2,6 +2,10 @@ package Logger;
 
 import java.util.Comparator;
 
+/**
+ * Interface Kommentiert
+ * @param <T>
+ */
 public interface GenerischesInterface <T> extends Comparator<T> {
 
      int compare(T a, T b);
