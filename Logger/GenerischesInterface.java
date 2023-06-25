b0ac1ca173a9458762601aa64e0e853e9778980b
derPascalD <pascal2122@gmx.de>
2023-06-25 16:01:53 +0200

    Test

--- Logger/GenerischesInterface.java
+++ Logger/GenerischesInterface.java
@@ -7,6 +7,11 @@ import java.util.Comparator;
  * @param <T>
  */
 public interface GenerischesInterface <T> extends Comparator<T> {
-
+     /**
+      *
+      * @param a the first object to be compared.
+      * @param b the second object to be compared.
+      * @return
+      */
      int compare(T a, T b);
 }
