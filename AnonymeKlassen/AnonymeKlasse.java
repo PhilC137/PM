f20772e61ecc71d82538ed6964a2220d3b6a5419
derPascalD <pascal2122@gmx.de>
2023-06-28 00:09:50 +0200

    add Anonyme Klassen

---
+++ AnonymeKlassen/AnonymeKlasse.java
@@ -0,0 +1,31 @@
+package AnonymeKlassen;
+
+public class AnonymeKlasse {
+    public static void main(String[] args){
+
+        //1. Klasse erweitern
+        Katze katze = new Katze(){
+            @Override
+            public void miau() {
+                System.out.println("Wuf");;
+            }
+        };
+        katze.miau();
+    }
+
+    //2. Interface Implementieren
+    Lebewesen affe = new Lebewesen() {
+        @Override
+        public void machEtwas() {
+            System.out.println("Uh Ah");
+        }
+    };
+
+}
+
+
+class Katze {
+    public void miau(){
+        System.out.println("Miau");
+    }
+}
